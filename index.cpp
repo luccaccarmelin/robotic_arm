1e7637b0dfccadbcd4911dbded9e554c403baa8b
Lucca Vinicius Cardoso Carmelin <81098582+luccaccarmelin@users.noreply.github.com>
2022-01-16 15:18:59 -0300

    added joystick class

--- index.cpp
+++ index.cpp
@@ -1,3 +1,23 @@
-//libraries
+// libraries
 #include <EEPROM.h>
 #include <Servo.h>
+
+//---------------------------------------------------------
+//class joystick
+
+class Joystick {
+  
+  public:
+  
+  Joystick(int pinX,int pinY,int pinBtn = -1){
+    _pinX = pinX 
+    _pinY = pinY
+    _pinBtn = pinBtn
+    pinMode(_pinX, INPUT);
+    pinMode(_pinY, INPUT);
+    if(_pinBtn > -1)
+      pinMode(_pinBtn, INPUT_PULLUP);
+
+    _meanX = 500;
+    _meanY = 500;
+  }
