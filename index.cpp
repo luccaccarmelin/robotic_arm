79c5d5974128c3c11961e516cfc10a2cd931e536
Lucca Vinicius Cardoso Carmelin <81098582+luccaccarmelin@users.noreply.github.com>
2022-02-06 18:18:27 -0300

    Update index.cpp

--- index.cpp
+++ index.cpp
@@ -150,6 +150,137 @@ class Joystick {
 					 int _limits[2];
 					 int _pin;
 
+					 int getLimit(byte index){
+						 if(index == ROBOSERVO_MAX){
+							 return _limits[ROBOSERVO_MAX];
+						 }	 else {
+							 return _limits[ROBOSERVO_MIN];
+							}
+				        }
+
+
+
+
+
+				  	void setLimit(byte index, int value){
+						if(index > ROBOSERVO_MAX)
+
+
+						if(value < 0)
+							value = 0;
+						if(value > 180)
+							value = 180;
+
+						_limits[index] = value;
+
+						if(_limits[ROBOSERVO_MAX] < _limits[ROBOSERVO_MIN]){
+							int
+							_limits[ROBOSERVO_MIN] = _limits[ROBOSERVO_MAX];
+							_limits[ROBOSERVO_MIN] = value;
+						}
+					     }
+				 };
+
+				#undef ROBOSERVO_MIN
+				#undef ROBOSERVO_MAX
+
+				#define JOYSTICK_DEADBAND  200
+
+				 joystick jLeft(A2, A1, A0,);
+				 joystick jRigth(A5, A4, A3,);
+				
+				 #define SERVO_STEP 5
+				 enum Motor {Base , Reach , Heigth , Claw};
+				 Roboservo servos[] = {RoboServo(8) , RoboServo(9) , RoboServo(10) , Roboservo(11)};
+				 const int delay_loop = 20;
+				 int current_motor = Motor::Base;
+				 int jValue;
+				 int AngleCorrection(Motor, int);
+				 void ReadConfig(void);
+
+				 void setup(){
+					 Serial.begin(115200);
+					 Serial.println(F("RoboARM - Demo"));
+					 Serial.println(F("\t\t10/12/18\n"));
+					 #ifndef RESET_CONFIG
+					   ReadConfig();
+					 #endif
+
+					   jLeft.calibrate(&Serial);
+					   jRigth.calibrate(&Serial);
+
+					   for(int i=0 ; i <= Motor::Claw ; i++){
+						   servos[i].attach();
+						 }
+				 	      }
+
+				 	      void loop(){
+
+						      if(Serial.available()){
+							      char c = Serial.read();
+							      switch(c){
+								 case 'i': {
+								  Serial.print(F("left: "));
+								  jleft.printTo(Serial);
+								  Serial.print(F("Rigth: "));
+								  jRigth.printTo(Serial);
+								  Serial.print(F("-----"));
+								  for(int i=0 ; i <= Motor::Claw ; i++){
+									  Serial.print('[');
+	    Serial.print(i);
+	    Serial.print("] ");
+	    Servos[i].printTo(Serial);
+	  }
+	  break;
+	}
+
+        case '1':
+	    current_motor = Motor::Base;
+	    Serial.println(F("Base selected"));
+	    break;
+	case '2':
+	    current_motor = Motor::Reach;
+	    Serial.println(F("Reach selected"));
+	    break;
+	case '3':
+	    current_motor = Motor::Heigth;
+	    Serial.println(F("Heigth selected"));
+	    break;
+	case '4':
+	    current_motor = Motor::Claw;
+	    Serial.println(F("Claw selected"));
+	    break;
+
+
+	case 'a': {
+			  int angle = servos[current_motor].read();
+			  Serial.print(F("Current angle: "));
+			  Serial.println(angle);
+			  break;
+			}
+
+	case '+': {
+			  int angle = servos[current_motor].read();
+			  angle += SERVO_STEP
+			  angle = angleCorrection(current_motor, angle);
+			  servos[current_motor].write(angle);
+			  Serial.print(F("current angle: "));
+			  Serial.println(angle);
+			  break;
+			}
+
+       case '<': {
+			 int angle = servos[current_motor].read();
+			 servos[current_motor].setMIN(angle);
+			 servos[current_motor].printTo(Serial);
+			 break;
+		       }
+
+
+
+
+
+
 
 
 
